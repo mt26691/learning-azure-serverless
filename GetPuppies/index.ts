9eea9a0be436395c7a5263f29153a16b21fc9a36
mt26691 <tung.nguyen@scopicsoftware.com>
2020-01-30 20:17:50 +0700

    connect to db

--- GetPuppies/index.ts
+++ GetPuppies/index.ts
@@ -1,21 +1,35 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
+import { MongoClient } from 'mongodb';
+const auth = {
+    user: process.env.CosmosDBUser,
+    password: process.env.CosmosDBPassword
+}
 
-    if (name) {
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+
+    
+    MongoClient.connect(`mongodb://${process.env.CosmosDBUser}:${process.env.CosmosDBPassword}@mt2606.documents.azure.com:10255/?ssl=true`, { auth: auth }, function (err, db) {
+        if (err) {
+            context.res = {
+                status: 400,
+                body: "Can't connect"
+            };
+            throw err;
+        }
+
+        console.log('Connected mongodb client');
+
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Connected mongodb client"
         };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+        db.close();
+        context.done();
+    });
+
+
 };
 
 export default httpTrigger;
